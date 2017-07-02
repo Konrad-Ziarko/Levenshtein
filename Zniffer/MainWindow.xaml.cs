b04ffa050278f7ddeb951042bc4b35352843ef47
Konrad <konrad19931118@gmail.com>
2017-07-03 01:34:03 +0200

    Szablon okna aplikacji

---
+++ Zniffer/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using Zniffer.Network;
+
+namespace Zniffer {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        #region TitleBar buttons
+        private void button_Exit_Click(object sender, RoutedEventArgs e) {
+            Close();
+        }
+
+        private void button_Max_Click(object sender, RoutedEventArgs e) {
+            if (WindowState == WindowState.Maximized)
+                WindowState = WindowState.Normal;
+            else
+                WindowState = WindowState.Maximized;
+        }
+
+        private void button_Min_Click(object sender, RoutedEventArgs e) {
+            WindowState = WindowState.Minimized;
+        }
+        #endregion
+
+        public MainWindow() {
+
+            Sniffer snf = new Sniffer();
+        }
+
+    }
+}
---
+++ Zniffer/Network/Sniffer.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Sockets;
+
+
+
+namespace Zniffer.Network {
+    class Sniffer {
+        private Socket mainSocket;                          //The socket which captures all incoming packets
+        private byte[] byteData = new byte[4096];
+        private bool bContinueCapturing = false;            //A flag to check if packets are to be captured or not
+
+
+        public Sniffer() {
+
+        }
+    }
+}
